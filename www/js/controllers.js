92a4556ed0a2cccfafcf193a9e2f6d15c3b44a55
peysal <peysal@gmail.com>
2015-04-25 09:55:51 +0800

    Basic modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,41 @@
 angular.module('parkey.controllers', [])
 
-.controller('LoginCtrl', function($scope) {})
+.controller('LoginCtrl', function ($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+.controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('SearchCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('SearchCtrl', function ($scope, $ionicModal) {
+    $ionicModal.fromTemplateUrl('tab-search-modal-result.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+    $scope.openModal = function () {
+        $scope.modal.show();
+    };
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+        $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function () {
+        // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function () {
+        // Execute action
+    });
 });
