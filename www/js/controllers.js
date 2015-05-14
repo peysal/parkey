e63f756605be7bf8ebea36bfa40b1282249f1e8f
peysal <peysal@gmail.com>
2015-05-14 12:23:11 +0800

    Persistant is wrap as a service.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,11 @@
 angular.module('parkey.controllers', [])
 
-.controller('LoginCtrl', function ($scope, $state) {
+.controller('LoginCtrl', function ($scope, $state, login) {
     $scope.authentication = {};
     $scope.login = function () {
         $scope.authentication.loggedIn = true;
         console.log('LoginCtrl-> login:' + $scope.authentication.loggedIn);
-        Parse.initialize("il1qy2ZyYoYtI7oXckZXVaELaeLsaxlVjHnRSy6Q", "ShLwaR6OOE2qy8M0nnMXdxNt4i75Qk9A5e6hD9Mc");
-
-        var TestObject = Parse.Object.extend("TestObject");
-        var testObject = new TestObject();
-        testObject.save({
-            foo: "bar"
-        }).then(function (object) {
-            console.log("yay! it worked");
-        });
-
+        login();
         $state.go('tab.logged');
     };
 })
